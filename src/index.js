068928a6c0bda47e7bf5c8e510fc6bf3f8925103
Evgenii Petukhov <evgeniy.petukhov@arcadia.spb.ru>
2022-05-17 01:56:29 +0200

    Added more samples

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ import _ from 'lodash';
 import TeXToSVG from './tex-to-svg';
 
 function component() {
-  const svg = TeXToSVG('\\sqrt[\\sqrt[]{}]{}', {
+  const div = document.createElement('div');
+
+  div.appendChild(getImageElement('\\frac{1}{2}'));
+  div.appendChild(document.createElement('br'));
+  div.appendChild(getImageElement('\\sqrt[3]{8}'));
+  div.appendChild(document.createElement('br'));
+  div.appendChild(getImageElement('\\sqrt[\\sqrt[]{}]{}'));
+
+  return div;
+}
+
+function getImageElement(tex) {
+  const svgSqrt = TeXToSVG(tex, {
     width: 1280,
     ex: 8,
     em: 16,
   });
-  console.log(svg);
   const element = document.createElement('img');
-  element.src = `data:image/svg+xml;charset=utf8,${encodeURIComponent(svg)}`;
+  element.src = `data:image/svg+xml;charset=utf8,${encodeURIComponent(svgSqrt)}`;
 
   return element;
 }
