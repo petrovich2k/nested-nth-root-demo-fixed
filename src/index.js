dd1d7197a885b3f0e507d1fa2204ca9b70b8d06d
Evgenii Petukhov <evgeniy.petukhov@arcadia.spb.ru>
2022-05-17 01:44:15 +0200

    Fixed tar archived: missing scripts

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import TeXToSVG from './tex-to-svg';
 
 function component() {
-  const svg = TeXToSVG('\\sqrt[]{}', {
+  const svg = TeXToSVG('\\sqrt[\\sqrt[]{}]{}', {
     width: 1280,
     ex: 8,
     em: 16,
